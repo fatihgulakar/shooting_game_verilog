5830cb4adb529e805f3cb25be709415309417ba6
Fatih Gulakar <gulakar15@itu.edu.tr>
2022-06-13 22:08:40 +0300

    Added game to user_proj_example

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -75,20 +75,6 @@ module user_proj_example #(
     wire [`MPRJ_IO_PADS-1:0] io_out;
     wire [`MPRJ_IO_PADS-1:0] io_oeb;
 
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
-
     // IO
     assign io_out = count;
     assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
@@ -104,62 +90,28 @@ module user_proj_example #(
     assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
     assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
 
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
+    wire ps2_data    = io_in[0];
+    wire ps2_clk     = io_in[1];
+    wire start       = io_in[2];
+    wire hsync       = io_out[0];
+    wire vsync       = io_out[1];
+    wire [2:0] red   = io_out[2 +: 3];
+    wire [2:0] green = io_out[5 +: 3];
+    wire [1:0] blue  = io_out[9 +: 2];
+
+    main shooting_game(
+        .ps2_data   (ps2_data),
+        .ps2_clk    (ps2_clk),
+        .board_clk  (clk),
+        .start      (start),
+        .reset      (rst),
+        .hsync      (hsync),
+        .vsync      (vsync),
+        .red        (red),
+        .green      (green),
+        .blue       (blue)
     );
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
-
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
-
-endmodule
 `default_nettype wire
